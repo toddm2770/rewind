3c63c0567e6acc6bc95e60b71c7f64789288ebd5
Todd <toddm2770@gmail.com>
2024-07-17 12:48:44 -0400

    fixed logo and added code page

--- js/site.js
+++ js/site.js
@@ -1,15 +1,36 @@
-
-
+//get values from the screen and display them
 function getValues(){
-    alert("Hello World");
+    let inputMessage = document.getElementById("message").value;
+    let alertMessage = document.getElementById("alertMessage");
+
+    let reverseMessage = reverse(inputMessage);
+
+    alertMessage.innerText = reverse(inputMessage);
+
+    alertMessage.innerHTML = displayMessage(inputMessage, reverseMessage)
+
 }
 
+//return the message in reverse
+function reverse(message){
+    let reverseMessage = "";
 
-//get values from the screen and display them
-function getValues2(){
-    let inputMessage = document.getElementById("message").value;
+    for (let index = message.length - 1; index >= 0; index--){
+        reverseMessage += message[index];
+    }
+
+    return reverseMessage;
+}
+
+//displays inputMessage and reverseMessage. Makes "alertMessage" visable
+function displayMessage(inputMessage, reverseMessage){
+    let alertElement = document.getElementById("alertMessage");
+    alertElement.style.opacity = 1;
+
+
+
+    let message = `<div>The message, <strong>${inputMessage}</strong>, reversed is, <strong>${reverseMessage}</strong></div>`;
     
-    let alertMessage = document.getElementById("alertMessage");
-    alertMessage.innerText = inputMessage;
-
+    
+    return message;
 }
